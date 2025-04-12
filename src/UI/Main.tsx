86259f08e13ea4a931aa0ca9a77674a7518febac
BrankoMuruaga <muruagabranko253@gmail.com>
2025-04-12 13:59:24 -0300

    Agrega el componente GitButton y la constante URL_GITHUB, e implementa el icono de Github.

--- src/UI/Main.tsx
+++ src/UI/Main.tsx
@@ -1,3 +1,4 @@
+import GitButton from "@src/components/GitButton";
 import { Editor } from "@src/UI/Editor";
 import Aside from "@UI/Aside";
 
@@ -6,6 +7,7 @@ function Main() {
     <main className="bg-transparent h-full w-4/6 py-10 m-auto flex justify-center items-start">
       <Aside />
       <Editor />
+      <GitButton />
     </main>
   );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_TITLE = "Untitled document";
 export const DEFAULT_ID = "StructNotes_doc_";
+export const URL_GITHUB = "https://github.com/BrankoMuruaga/StructNotes";
