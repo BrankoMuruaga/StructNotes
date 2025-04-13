ba041c71f7faea121c148c941286f55136a05063
BrankoMuruaga <muruagabranko253@gmail.com>
2025-04-13 12:53:19 -0300

    Actualiza los iconos Check y X para permitir la personalización del color de trazo. Mejora el componente SaveButton para reflejar los cambios de estado con colores específicos.

--- src/UI/Navbar.tsx
+++ src/UI/Navbar.tsx
@@ -6,9 +6,9 @@ import SaveButton from "@src/components/SaveButton";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 z-15 flex bg-secondary-light dark:bg-secondary-dark w-full py-1  rounded-b-xl ">
+    <nav className="fixed top-0 z-15 flex bg-secondary-light dark:bg-secondary-dark w-full py-1 rounded-b-xl ">
       <MenuButton className="z-20 ml-3" />
-      <section className="fixed bottom-10 w-full h-10 z-10 sm:block sm:top-0 sm:w-1/6 flex justify-center ">
+      <section className="fixed bottom-10 w-full h-10 z-10 sm:block sm:top-0 sm:w-1/6 flex justify-center sm:ml-16 ">
         <div className="flex justify-around items-center w-1/2 bg-secondary-light dark:bg-secondary-dark sm:bg-transparent sm:dark:bg-transparent rounded-full py-2 sm:py-1 px-3 sm:gap-7 ">
           <section className="flex">
             <HistoryButton undo />
